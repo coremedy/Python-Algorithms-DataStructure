bdc5c321d81b5c945e7886cf4947f51d65f88176
coremedy <coremedy@hotmail.com>
2015-08-04 21:33:41 +1000

    Need to pay attention

--- Python-Algorithms-DataStructure/src/leet/105_ConstructBinaryTreefromPreorderandInorderTraversal.py
+++ Python-Algorithms-DataStructure/src/leet/105_ConstructBinaryTreefromPreorderandInorderTraversal.py
@@ -25,5 +25,21 @@ class Solution:
         node.right = self.buildTree(preorder, inorder[index + 1:])
         return node
 
+    def DFS(self, preorder, preorder_beg, preorder_end, inorder, inorder_beg, inorder_end):
+        if preorder_beg > preorder_end or inorder_beg > inorder_end:
+            return None
+        key = preorder[preorder_beg]
+        index = self.BruteForceSearch(inorder, inorder_beg, inorder_end, key)
+        node = TreeNode(key)
+        node.left = self.DFS(preorder, preorder_beg + 1, preorder_beg + index - inorder_beg, inorder, inorder_beg, index - 1)
+        node.right = self.DFS(preorder, preorder_end + 1 + index - inorder_end, preorder_end, inorder, index + 1, inorder_end)
+        return node
+        
+    def BruteForceSearch(self, array, beg, end, key):
+        for index in range(beg, end + 1):
+            if array[index] == key:
+                return index
+        return -1
+
 if __name__ == '__main__':
     pass
