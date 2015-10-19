2246ab1b2f01ffe954f9722b185fc45bc1a63e30
coremedy <coremedy@hotmail.com>
2015-10-20 07:22:01 +1100

    Need to pay attention

---
+++ Python-Algorithms-DataStructure/src/leet/240_Searcha2DMatrixII.py
@@ -0,0 +1,21 @@
+'''
+Created on 2015-10-20
+'''
+
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        m, c = len(matrix), len(matrix[0]) - 1 
+        for r in range(m):
+            while c >= 0 and matrix[r][c] > target:
+                c -= 1
+            if matrix[r][c] == target:
+                return True
+        return False
+
+if __name__ == '__main__':
+    pass
